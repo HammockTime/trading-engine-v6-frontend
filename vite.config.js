11bd3c660db0d74555e6a37eefef6ad8adb82f59
HammockTime <mcg.ross.uk@gmail.com>
2025-05-19 21:45:39 +1200

    Update Vite configuration with explicit root

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react() ],
+  root: '.', // Explicitly set the root directory
   build: {
     outDir: 'dist',
   },
+  plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
